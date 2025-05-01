ccd4b205e7dd30ebb52a85b5b721e05682bfc031
ishikajindal13 <ishika082004@gmail.com>
2025-05-01 20:33:25 +0530

    first commit done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h2>This is react app implement with ci-cd Pinpeline</h2>
+      <h1> my raect app</h1>
+      <h1> done by ishika day-3</h1>
+      <h3> With github action</h3>
+    </div>
+  )
+}
+
+export default App
